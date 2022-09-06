74f22e53f8468d51d9d4749f4b51a02bdead5ebe
VinhDink <97300561+VinhDink@users.noreply.github.com>
2022-09-07 01:16:29 +0700

    dd

--- app/login.php
+++ app/login.php
@@ -25,7 +25,6 @@ function check_if_username_exist($login_name,$login_pass) {
     if (in_array($login_name,$username)) {
         $user_pos = array_search($login_name,$username);
         if(password_verify($login_pass,$password[$user_pos])) {
-            print_r("$login_pass");
             return true;
         }return false;
     }   
