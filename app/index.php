2835a5286dd53bf2c6de293889978f0f29d75dc8
DuyN <DuyN@eastagile.com>
2022-09-08 08:44:19 +0700

    refactor product page and add page

--- app/index.php
+++ app/index.php
@@ -2,34 +2,37 @@
 <html lang="en">
 <head>
     <?php
-    include 'partials/header.php';
-
-    $product_json = file_get_contents('db/product.db');
-    $products_array = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $product_json), true);
+        include 'partials/header.php';
     ?>
     <link rel="stylesheet" href="styles/pages/products.css">
 </head>
 <body>
-<h1>Products</h1>
+    <div>
+        <h1>Products</h1>
+        <button class="btn btn-md" onclick="navigateTo('product/add.php')">Add new</button>
+    </div>
 <div class="productList grid grid-col-3 gap-lg">
     <?php foreach ($products_array as $a) { ?>
         <div class="productItem flex gap-md items-center">
-            <div>
-                <img src="<?php echo $a['img'] ?>" alt="" class="rad-md" style="width: 8rem; height: 8rem"/>
-            </div>
-            <div class="flex gap-md items-center">
-                <div class="flex flex-col gap-sm">
-                    <h4><?php echo $a['short'] ?></h4>
-                    <p class="descriptionText"><?php echo $a['description'] ?></p>
+            <div class="productItemDescription">
+                <img 
+                    src="<?php echo empty($a['img'])? '' : $a['img']; ?>"
+                    alt="" 
+                    class="rad-md" 
+                    style="width: 8rem; height: 8rem"
+                />
+                <div class="flex flex-col gap-sm description">
+                    <h4><?php echo empty($a['short'])? '' : $a['short']; ?></h4>
+                    <p class="descriptionText"><?php echo empty($a['description'])? '' : $a['description']; ?></p>
                     <div class="flex gap-md">
                         <a href="#" class="btn btn-sm bg-red rad-xs text-white font-bold" onclick="addToCart('<?php echo $a['id'] ?>')">Add to Cart</a>
                         <button class="btn btn-sm bg-none font-medium" style="padding-left: 0;"><i class="fa-regular fa-heart text-red"></i> Add to Wish</button>
                     </div>
                 </div>
-                <div class="flex flex-col items-center" style="height: 100%">
-                    <div class="font-bold text-lg text-red" style="align-self: center"><?php echo $a['price'] ?> $</div>
-                    <button class="detailsButton btn btn-md" onclick="navigateToProduct(<?php echo $a['id'] ?>)">Details..</button>
-                </div>
+            </div>
+            <div class="flex flex-col items-center" style="height: 100%">
+                <div class="font-bold text-lg text-red" style="align-self: center"><?php echo empty($a['price'])? '' : $a['price']; ?> $</div>
+                <button class="detailsButton btn btn-md" onclick="navigateToProduct(<?php echo $a['id'] ?>)">Details..</button>
             </div>
         </div>
     <?php } ?>
@@ -45,10 +48,5 @@ include 'partials/footer.php';
 <script>
     function addToCart(id) {
         alert(id)
-    }
-
-    function navigateToProduct(id) {
-        sessionStorage.setItem('product_id', id);
-        window.location=`product/index.php`;
     }
 </script>
--- app/product/index.php
+++ app/product/index.php
@@ -1,42 +1,42 @@
 <?php
     include '../partials/header.php';
 
-    $product_json = file_get_contents('../db/product.db');
-    $array = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $product_json), true);
+    function findObjectById($id, $array){
+        if ( isset( $array[$id] ) ) {
+            return $array[$id];
+        }
+
+        return false;
+    }
 ?>
-<h1>Products</h1>
-<div class="grid grid-col-3 gap-lg">
-    <?php foreach ($array as $a) { ?>
-    <div class="prod flex gap-md items-center" onclick="navigateToProduct(<?php $a['id'] ?>)">
-      <!-- TODO: add image src via db -->
-        <img src="<?php echo $a['image'] ?>" alt="" class="rad-md" style="width: 8rem; height: 8rem"/>
-        <div class="flex gap-md">
-            <div class="flex flex-col gap-sm">
-                <h4><?php echo $a['name'] ?></h4>
-                <p><?php echo $a['description'] ?></p>
-                <div class="flex gap-md">
-                    <a href="#" class="btn btn-sm bg-red rad-xs text-white font-bold" onclick="addToCart('<?php echo $a['id'] ?>')">Add to Cart</a>
-                    <button class="btn btn-sm bg-none font-medium" style="padding-left: 0;"><i class="fa-regular fa-heart text-red"></i> Add to Wish</button>
-                </div>
-            </div>
-            <div class="flex flex-col" style="height: 100%">
-                <div class="price font-bold text-lg text-red" style="align-self: start"><?php echo $a['price'] ?></div>
-            </div>
-        </div>
+
+<?php 
+    $current_product = NULL;
+    if(isset($_COOKIE['product_id'])) {
+        $current_product = findObjectById($_COOKIE['product_id'], $products_array);
+    }
+?>
+
+<div>
+    <div>
+        <img 
+            src="<?php echo $current_product['img'] ?>" 
+            alt="" 
+            class="rad-md" 
+            style="width: 8rem; height: 8rem"
+        />
     </div>
-    <?php } ?>
+    <h1><?php echo empty($current_product['name'])? '' : $current_product['name']; ?></h1>
+    <p><?php echo empty($current_product['price'])? '' : $current_product['price']; ?></p>
+    <p><?php echo empty($current_product['description'])? '' : $current_product['description']; ?></p>
+    <p><?php echo empty($current_product['vendor'])? '' : $current_product['vendor']; ?></p>
+
+    <div class="flex gap-md">
+        <a href="#" class="btn btn-sm bg-red rad-xs text-white font-bold" onclick="addToCart('<?php echo $a['id'] ?>')">Add to Cart</a>
+        <button class="btn btn-sm bg-none font-medium" style="padding-left: 0;"><i class="fa-regular fa-heart text-red"></i> Add to Wish</button>
+    </div>
 </div>
 
 <?php
     include '../partials/footer.php';
 ?>
-
-<script>
-    function navigateToProduct(id) {
-        document.getElementById(id).style.visibility='visible';
-    }
-
-    function addToCart(id) {
-        alert(id)
-    }
-</script>
