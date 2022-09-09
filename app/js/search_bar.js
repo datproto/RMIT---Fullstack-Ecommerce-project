c7b395694e1a5263e53458d23badfb41d9fa885e
Nguyen Cong Chinh <s3926234@rmit.edu.vn>
2022-09-09 18:32:41 +0700

    commit

--- app/js/search_bar.js
+++ app/js/search_bar.js
@@ -1,28 +1,41 @@
-var input = document.getElementById("searchbar");
-input.addEventListener("keydown", function (enter) {
-    if (enter.key === "Enter") {
-        search()
-        }
-    }
-)
+
+// var input = document.getElementById("searchbar");
+// input.addEventListener("keydown", function (enter) {
+//     if (enter.key === "Enter") {
+//         search()
+//         }
+//     }
+// )
+//
+//
+// function search() {
+//     // Get the user's input from the page
+//     var searchQuery = document.getElementById("searchbar").value;
+//
+//     // Get the item from LocalStorage
+//     var localStorageItem = localStorage.getItem(searchQuery);
+//
+//     // Do your DOM manipulation magic
+//     if (localStorageItem !== null){
+//         var elementValue = $('#MyTable')
+//             .find('tr#key_' + localStorageItem)         // code for filter
+//             .children('td.two')
+//             .text();
+//         console.log(elementValue);
+//     } else {
+//         alert("SORRY THIS ITEM IS NOT AVAILABLE")
+//     }
+// }
+//
+
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value;
+    console.log(value);
+})
 
 
-function search() {
-    // Get the user's input from the page
-    var searchQuery = document.getElementById("searchbar").value;
-
-    // Get the item from LocalStorage
-    var localStorageItem = localStorage.getItem(searchQuery);
-
-    // Do your DOM manipulation magic
-    if (localStorageItem !== null){
-        var elementValue = $('#MyTable')
-            .find('tr#key_' + localStorageItem)         // code for filter
-            .children('td.two')
-            .text();
-        console.log(elementValue);
-    } else {
-        alert("SORRY THIS ITEM IS NOT AVAILABLE")
-    }
-}
 
