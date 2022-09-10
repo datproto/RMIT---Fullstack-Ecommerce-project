72ad65a5ce8fe0b7f67208d4297fd59fd63c2a0f
Dat Proto <contact@datproto.com>
2022-09-11 02:57:32 +0700

    FINISH: Checkout

--- app/shopping_cart.php
+++ app/shopping_cart.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['buy_prod'])) {
 }
 
 $displayed_cart_items = [];
+$total_price = 0;
 foreach ($cart_items_array as $item) {
     $displayed_cart_items[] = get_item('id', $item['prod'], $products_array)[0];
 }
@@ -26,7 +27,10 @@ foreach ($cart_items_array as $item) {
 <div class="flex flex-col lg:flex-row gap-md">
     <div class="flex-1 flex flex-col gap-md rad-sm" style="padding: 1rem; border: 1px solid gray">
         <!-- Product -->
-        <?php foreach ($displayed_cart_items as $key=>$item) { ?>
+        <?php
+            foreach ($displayed_cart_items as $key=>$item) {
+              $total_price += (int)ltrim(str_replace(',','',$item->price), '₫');
+        ?>
             <div id="prod-<?php echo $key ?>" class="w-full flex items-center justify-between">
                 <div class="information flex-1 flex items-center gap-md">
                     <img src="<?php echo $item->img ?>" alt="" class="w-8xl rad-md" style="object-fit: cover">
@@ -43,13 +47,14 @@ foreach ($cart_items_array as $item) {
         <h4>Shipping Details</h4>
         <div class="flex justify-between gap-sm">
             <p>Total price</p>
-            <p class="font-medium text-red">500000</p>
+            <p class="font-medium text-red"><?php echo $total_price ?></p>
         </div>
         <div class="flex justify-between gap-sm">
             <p>Address</p>
-            <p class="font-medium text-red">RMIT University</p>
+            <p class="font-medium text-red"><?php echo $b_address ?></p>
         </div>
-        <button class="btn btn-md bg-red text-white font-bold rad-sm">Check out</button>
+        <button class="btn btn-md bg-red text-white font-bold rad-sm"
+                onclick="checkOut()">Check out</button>
     </div>
 </div>
 <script>
@@ -61,6 +66,28 @@ function unsetCardProd(key) {
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.send("remove_prod=" + encodeURIComponent(JSON.stringify(key)));
   window.location.reload();
+}
+
+function checkOut() {
+  let data = {
+    username: '<?php echo $username ?>',
+    hub_name: 'hub2',
+    prods: '<?php echo json_encode($_SESSION['buy_prod']) ?>',
+    address: '<?php echo $b_address ?>',
+    active: true
+  }
+  let url = "shopping_cartCheckOut.php";
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.onreadystatechange = function() { if (xhr.readyState === 4 && xhr.status === 200) { console.log(xhr.responseText); } }
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("checkout=" + encodeURIComponent(JSON.stringify(data)));
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      console.log(xhr.response);
+    }
+  }
+  // window.location.reload();
 }
 </script>
 <?php
