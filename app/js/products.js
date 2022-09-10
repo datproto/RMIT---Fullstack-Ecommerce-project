224daea0882fd930600a5abe8061a86c62ab5b79
DuyN <DuyN@eastagile.com>
2022-09-10 17:45:32 +0700

    merge main and refactor shop and product

--- app/js/products.js
+++ app/js/products.js
@@ -1,4 +1,6 @@
-const navigateToProduct = (id) => {
-  document.cookie = "product_id = " + id;
-  navigateTo(`product/index.php`)
+function addToCart(uid = 1, prod_id) {
+  addLocalStorage("buy_prod", {
+      user: uid,
+      prod: prod_id
+  }, 'array', 'append')
 }
--- app/partials/header.php
+++ app/partials/header.php
@@ -148,6 +148,8 @@
         $b_address  = $curr_user->b_address;
         $avatar     = $curr_user->avatar;
     }
+
+    $products_array = read($path.'/db/lazada.db');
 ?>
 <!doctype html>
 <html lang="en">
@@ -162,6 +164,7 @@
     <script src="https://kit.fontawesome.com/25adc7f9b5.js" crossorigin="anonymous"></script>
     <script src="../js/addLocalStorage.js"></script>
     <script src="../js/removeLocalStorage.js"></script>
+    <script src="../js/products.js"></script>
 </head>
 <body>
     <header>
