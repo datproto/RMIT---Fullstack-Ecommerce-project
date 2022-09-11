e1c03a938d270bfa08927d1b91b0bc785fb26751
VinhDink <97300561+VinhDink@users.noreply.github.com>
2022-09-11 22:48:43 +0700

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,45 +2,31 @@
     require('config.php');
     include($path.'/partials/header.php');
 
-    function check_if_username_exist($login_name,$login_pass): bool
-    {
-        $users      = read("db/accounts.db");
-
-        $curr_user = get_item('uname',$login_name,json_decode($users))[0];
-        if ($curr_user) {
-            $password   = $curr_user->pass;
-            $role       = $curr_user->role;
-            
-
-            if(password_verify($login_pass,$password)) {
-                if ($role == "vendor") {
-                    $_SESSION["role"]="vendor";
-                }
-                elseif($role == "customer") {
-                    $_SESSION["role"]="customer";
-                    //go to customer page
-                }
-                else {
-                    $_SESSION["role"]="customer";
-                    //go to shipper page
-                }
-            } else {
-                echo 'Wrong password!';
-                return false;
-            }
-        } else {
-            echo 'Cannot find user!';
-            return false;
-        }
-        return true;
+    function check_if_username_exist($login_name,$login_pass) {
+        $file = fopen("db/accounts.db","r");
+        $username=array();
+        $password=array();
+        while (($data = fgetcsv($file)) !== FALSE) {
+            array_push($username,$data[1]);
+            array_push($password,$data[2]);
+        }    
+        if (in_array($login_name,$username)) {
+            $user_pos = array_search($login_name,$username);
+            if(password_verify($login_pass,$password[$user_pos])) {
+                print_r("$login_pass");
+                return true;
+            }return false;
+        }   
     }
-
     if (isset($_GET['submit'])) {
         $login_name=$_GET["login_name"];
         $login_pass=$_GET["login_pass"];
-        if(check_if_username_exist($login_name, $login_pass)) {
+        if(check_if_username_exist($login_name, $login_pass) == true) {
             $_SESSION["u_name"] = $login_name;
             $_SESSION["logged"] = true;
+            ?>
+            <script src="./js/redirect.js"></script>       
+        <?php       
         } else {
             echo "Login failed";
         }
@@ -48,7 +34,7 @@
 ?>
 <h1 class="heading-center">Login</h1>
 <div class="center w-full md:w-1/2 lg:w-1/3">
-    <form method="get" action="my_account.php" class="flex flex-col items-center gap-md w-full">
+    <form method="get" action="" class="flex flex-col items-center gap-md w-full">
         <div class="w-full flex items-center gap-sm">
             <label class="register-input w-10xl font-medium text-red" for="login_name">Username:</label>
             <input class="register-input w-full" id="login_name" name="login_name" type="text" style="flex: 1"/>
